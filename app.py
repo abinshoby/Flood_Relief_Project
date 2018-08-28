effb4d37827d56c9917b2cf0d45a0e401fe9575e
abin <abinshoby@gmail.com>
2018-08-28 22:28:27 +0530

    integrated register.html

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask import *
 import os
 
 
+
 app = Flask(__name__)
 
 
@@ -28,5 +29,41 @@ def req1():
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
+@app.route('/register')
+def second():
+    return render_template('register.html')
+
+
+@app.route("/upload", methods=["POST"])
+def upload():
+    """Handle the upload of a file."""
+    form = request.form
+
+    # Create a unique "session ID" for this particular batch of uploads.
+    # upload_key = str(uuid4())
+
+    # Is the upload using Ajax, or a direct POST by the form?
+    is_ajax = False
+    if form.get("__ajax", None) == "true":
+        is_ajax = True
+
+
+    print("=== Form Data ===")
+    details = list()
+    for key, value in list(form.items()):
+        details.append(value)
+    f = open("../data/details.csv", "a")
+    f.write("\n0\t" + details[0].strip()   + "\t" + details[3].strip()+ "\t" + details[2].strip() + "\t" + details[1].strip())#additional message removed
+
+
+    return redirect(url_for("upload"))
+
+
+@app.route('/upload')
+def upload_complete():
+    return render_template(
+        'FloodRelief.html')  # abin shoby change your search python code so that it ll able to search for the input name and return
+
+
 if __name__ == '__main__':
     app.run(debug=True)
